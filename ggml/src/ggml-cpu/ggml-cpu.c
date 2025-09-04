ed6f54ca0728e50997dbc1b174626302dfd77dcc
leejet <leejet714@gmail.com>
2025-09-04 16:38:49 +0800

    ggml: add ops for WAN video model (cuda && cpu) (#15669)
    * add conv3d support
    
    * add ggml_pad_ext for cpu & cuda backend
    
    * cuda/cpu: add im2col_3d support
    
    * cuda: make im2col a little faster
    
    * fix cuda pad/scale/im2col3d
    
    * make im2col_3d faster
    
    * gguf: support loading tensors which n_dims > GGML_MAX_DIMS
    
    * fix cuda get_rows
    
    * avoid ggml_conv_3d conflict
    
    * correct GGML_OP_COUNT assertion
    
    * avoid build failure
    
    * avoid build failure on MacOS
    
    * cuda: remove unnecessary MIN define
    
    * fix cpu im2col_3d
    
    * adjust the code style
    
    * cuda: use simpler loop in get_rows
    
    * add test_im2col_3d to test-backend-ops
    
    * test-backend-ops.cpp: remove trailing whitespace
    
    * cpu: im2col_3d support non continuous src
    
    Co-authored-by: Jeff Bolz <jbolz@nvidia.com>
    
    * fix test_im2col_3d
    
    * remove unused variables
    
    * cuda: get_rows: dfloat2 -> float2
    
    * add test_pad_ext to test-backend-ops.cpp
    
    * add gguf_init_from_file_ext impl
    
    * Revert "gguf: support loading tensors which n_dims > GGML_MAX_DIMS"
    
    This reverts commit d8377a0a37f314bd3713fe043b4333ad661610c1.
    
    * Revert "add gguf_init_from_file_ext impl"
    
    This reverts commit d9f1d13208c68ef83b3538201ac7f31614fb1994.
    
    * update ggml_backend_vk_device_supports_op
    
    * fix ggml_backend_vk_device_supports_op
    
    * update other backend supports op for ggml_pad_ext
    
    * metal/opencl/sycl/vulkan: fix GGML_OP_PAD check in supports_op
    
    ---------
    
    Co-authored-by: Jeff Bolz <jbolz@nvidia.com>

--- ggml/src/ggml-cpu/ggml-cpu.c
+++ ggml/src/ggml-cpu/ggml-cpu.c
@@ -1925,6 +1925,10 @@ static void ggml_compute_forward(struct ggml_compute_params * params, struct ggm
             {
                 ggml_compute_forward_im2col_back_f32(params, tensor);
             } break;
+        case GGML_OP_IM2COL_3D:
+            {
+                ggml_compute_forward_im2col_3d(params, tensor);
+            } break;
         case GGML_OP_CONV_2D:
             {
                 ggml_compute_forward_conv_2d(params, tensor);
@@ -2326,6 +2330,7 @@ static int ggml_get_n_tasks(struct ggml_tensor * node, int n_threads) {
             } break;
         case GGML_OP_IM2COL:
         case GGML_OP_IM2COL_BACK:
+        case GGML_OP_IM2COL_3D:
         case GGML_OP_CONV_2D:
         case GGML_OP_CONV_3D:
         case GGML_OP_CONV_2D_DW:
